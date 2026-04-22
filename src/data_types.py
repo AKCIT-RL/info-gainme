4905373e8fd8f96a03ad08def0c12ffa6eed713b
Daniel Machado Pedrozo <dmachadopedrozo@gmail.com>
2026-04-22 19:42:08 -0300

    Revert "fix(oracle): revert prompt, allow 'Yes! You found the target!' in schema"
    This reverts commit 2e671d1c1346ccb2b4ee22a68d3f00b868028e9d.

--- src/data_types.py
+++ src/data_types.py
@@ -22,7 +22,7 @@ class Question:
 
 class OracleResponse(BaseModel):
     rationale: str
-    answer: Literal["Yes", "No", "Yes! You found the target!"]
+    answer: Literal["Yes", "No"]
     game_over: bool
 
 @dataclass
